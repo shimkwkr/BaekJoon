9288cdecbcb555e7bbec57121210aa6f1374938b
shimkwkr <133209685+shimkwkr@users.noreply.github.com>
2024-11-30 16:56:26 +0900

    [Gold IV] Title: 네트워크 연결, Time: 180 ms, Memory: 47804 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,51 @@
+# 1992. G4 네트워크 연결
+import sys
+input = sys.stdin.readline
+
+def make_set():
+    p = list(range(N+1))
+    return p
+
+def find_set(x):
+    if parents[x] == x:
+        return x
+
+    parents[x] = find_set(parents[x])
+    return parents[x]
+
+def union(x, y):
+    root_x = find_set( x )
+    root_y = find_set( y )
+
+    if root_x == root_y:
+        return
+
+    parents[root_y] = root_x
+
+def kruskal(edges, N):
+    cnt = 0
+    mst = []
+    for u, v, w in edges:
+        if find_set(u) != find_set(v):
+            cnt += 1
+            mst.append(w)
+            union(u, v)
+            if cnt == N-1:
+                break
+    return mst
+
+N = int(input())
+M = int(input())
+parents = make_set()
+edges = []
+
+for _ in range(M):
+    u, v, w = map(int ,input().split())
+    edges.append((u, v, w))
+
+edges.sort(key = lambda x: x[2])
+
+MST = kruskal(edges, N)
+
+print(sum(MST))
+
