df09fdf8a34e81e76247f6cc9db4e73f184ff199
shimkwkr <133209685+shimkwkr@users.noreply.github.com>
2025-07-19 22:35:44 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 644 ms, Memory: 36652 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -3,63 +3,69 @@ import java.util.*;
 
 public class Main {
 	
-	static int[] ans;
-	static boolean[] visit;
-	static boolean[] cycleNodes;
-	static List<Integer>[] graph;
+	static int[] distance;
+	static boolean[] visited;
+	static boolean[] isCycleNode;
+	static List<Integer>[] adjList;
 	
 	public static void main (String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine());		
-		ans = new int[N+1];
-		Arrays.fill(ans, -1);
+		int N = Integer.parseInt(br.readLine());
+		distance = new int[N+1];
+		Arrays.fill(distance, -1);
 		
-		graph = new ArrayList[N+1];
-		cycleNodes = new boolean[N+1];
+		isCycleNode = new boolean[N+1];
+		adjList = new ArrayList[N+1];
 		
-		for(int i=0; i<=N; i++) {
-			graph[i] = new ArrayList<>();
+		for (int i=0; i<=N; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());			
-			graph[u].add(v);
-			graph[v].add(u);
+			int v = Integer.parseInt(st.nextToken());
+			adjList[u].add(v);
+			adjList[v].add(u);
 		}
 		
+		// 어떤 노드가 Cycle에 포함되어 있는지 확인후 isCycleNode에 기록
 		for(int i=1; i<=N; i++) {
-			visit = new boolean[N+1];
-			if(isCycle(i, i, -1)){
-				cycleNodes[i]=true;
-				ans[i] = 0;
+			visited = new boolean[N+1];
+			
+			if(isCycle(i, i, -1)) {
+				isCycleNode[i] = true;
+				distance[i] = 0;
 			}
 		}
 		
-		bfsFromCycleNodes(N);
+		calculateDistanceFromCycle(N);
 		
-		for(int i=1; i<ans.length; i++) {
-			System.out.print(ans[i] + " ");
+		for(int i=1; i<=N; i++) {
+			System.out.print(distance[i] + " ");
 		}
 	}
 	
-	// 노드가 Cycle의 포함 여부 - DFS 이용
+	// Cycle에 포함되어 있는지 아닌지 - DFS
 	private static boolean isCycle(int start, int cur, int prev) {
-		visit[cur] = true;
+		visited[cur] = true;
 		
-		for (int next : graph[cur]) {
-			if (next == prev) continue;
+		for(int next : adjList[cur]) {
+			if (next == prev) {
+				continue;
+			}
 			
-			if (!visit[next]) {
+			// 아직 방문하지 않은 정점이라면?
+			if (!visited[next]) {
+				// 계속 DFS탐색을 진행
 				if(isCycle(start, next, cur)) {
-					return true;
+					return true;					
 				}
 			} else {
 				// 시작한 노드인데 직전 노드가 아닌 경우
-				if(next == start) {
+				if (next == start) {
 					return true;
 				}
 			}
@@ -67,12 +73,11 @@ public class Main {
 		return false;
 	}
 	
-	// Cycle로부터의 거리 구하기 - BFS이용
-	private static void bfsFromCycleNodes (int N) {
+	private static void calculateDistanceFromCycle(int N) {
 		Queue<Integer> q = new ArrayDeque<>();
-		for(int i=0; i<=N; i++) {
-			if(cycleNodes[i]) {
-				ans[i] = 0;
+		
+		for(int i=1; i<=N; i++) {
+			if(distance[i] == 0) {
 				q.offer(i);
 			}
 		}
@@ -80,12 +85,12 @@ public class Main {
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			
-			for(int next : graph[cur]) {
-				if (ans[next] == -1) {
-					ans[next] = ans[cur] + 1;
+			for(int next : adjList[cur]) {
+				if(distance[next] == -1) {
+					distance[next] = distance[cur] +1;
 					q.offer(next);
 				}
 			}
 		}
-	}	
+	}
 }
